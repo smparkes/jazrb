4cd9b1de14495daeb5cd7c7e3babb65dc2b09568
Steven Parkes <smparkes@smparkes.net>
2010-03-02 14:28:11 -0800

    rename

--- example/nested/SpecHelper.js
+++
@@ -1,2 +0,0 @@
-jazz_root = this.jazz_root || "../..";
-document.write("<script type='text/javascript' src='"+jazz_root+"/example/spec_helper.js'></script>");
--- spec/example/plain_spec.js
+++
@@ -1,8 +0,0 @@
-describe("truthy",function(){
-  
-  it("true should be truthy'",function(){
-    expect(!!true).toBe(true);
-  });
-
-});
-
